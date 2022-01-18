34786582614014a7e30050abc880b520df05ee05
ivanleoncz <ivanlmj@gmail.com>
2022-01-17 21:44:36 -0600

    Improving Admin Panel for "library"

--- apps/library/admin.py
+++ apps/library/admin.py
@@ -2,7 +2,35 @@ from django.contrib import admin
 
 from .models import Author, Book, Genre, BookInstance
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = (('first_name', 'last_name',),
+              'date_of_birth', 'date_of_death')
+
+    inlines = [BookInline]
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    fields = ('title', 'author', 'isbn', 'genre', 'summary')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
