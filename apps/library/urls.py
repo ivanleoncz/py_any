cf725410230eb2f6b283b2ea7c5de8147cced14f
ivanleoncz <ivanlmj@gmail.com>
2022-01-19 11:09:11 -0600

    Reviewing reverse URL definitions
    Also adding navigation from Library to Authors and Author detail.

--- apps/library/urls.py
+++ apps/library/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import LibraryView, AuthorListView, AuthorDetailView
 
 urlpatterns = [
-    path('library/', LibraryView.as_view(), name="library"),
-    path('library/author/', AuthorListView.as_view(), name="author_all"),
-    path('library/author/<int:pk>/', AuthorDetailView.as_view(), name="author")
+    path('library/', LibraryView.as_view(), name="library-view"),
+    path('library/author/', AuthorListView.as_view(), name="author-view"),
+    path('library/author/<int:pk>/', AuthorDetailView.as_view(), name="author-detail")
 ]
