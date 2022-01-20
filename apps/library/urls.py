f510c2dc45ba39013f0043526d389b5027b52e63
ivanleoncz <ivanlmj@gmail.com>
2022-01-19 20:59:43 -0600

    Adding "book" resource
    Views, templates and url definitions.

--- apps/library/urls.py
+++ apps/library/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import LibraryView, AuthorListView, AuthorDetailView
+from .views import LibraryView, AuthorListView, AuthorDetailView, BookListView, BookDetailView
 
 urlpatterns = [
     path('library/', LibraryView.as_view(), name="library-view"),
     path('library/author/', AuthorListView.as_view(), name="author-view"),
-    path('library/author/<int:pk>/', AuthorDetailView.as_view(), name="author-detail")
+    path('library/author/<int:pk>/', AuthorDetailView.as_view(), name="author-detail"),
+    path('library/book/', BookListView.as_view(), name="book-view"),
+    path('library/book/<int:pk>/', BookDetailView.as_view(), name="book-detail"),
 ]
--- apps/library/views.py
+++ apps/library/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Author
+from .models import Author, Book
 
 
 class LibraryView(generic.TemplateView):
@@ -20,4 +20,17 @@ class AuthorDetailView(generic.DetailView):
 
     model = Author
     template_name = "library/author_detail.html"
+
+
+class BookListView(generic.ListView):
+
+    model = Book
+    template_name = "library/book.html"
+    queryset = Book.objects.all().order_by('id')
+
+
+class BookDetailView(generic.DetailView):
+
+    model = Book
+    template_name = "library/book_detail.html"
 
