55a016841f62aaf24630c4a672d6ee5909ca406b
ivanleoncz <ivanlmj@gmail.com>
2022-01-22 17:18:20 -0600

    Adding extra context to /library/ resource
    Amount of books, authors and genres, used on a
    message for the home page.

--- apps/library/views.py
+++ apps/library/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Author, Book
+from .models import Author, Book, Genre
 
 
 class LibraryView(generic.TemplateView):
 
     template_name = "library/library.html"
+    extra_context = {
+        'books': Book.objects.all().count(),
+        'authors': Author.objects.all().count(),
+        'genres': Genre.objects.all().count()
+    }
 
 
 class AuthorListView(generic.ListView):
