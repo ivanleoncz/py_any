b07f449846f10409ca352c9e17401230f40abdcd
ivanleoncz <ivanlmj@gmail.com>
2022-02-01 22:11:04 -0600

    Adding default app structure for geotracking

---
+++ apps/geotracking/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GeotrackingConfig(AppConfig):
+    name = 'geotracking'
--- py_any/settings.py
+++ py_any/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'apps.cetes',
-    'apps.library'
+    'apps.library',
+    'apps.geotracking'
 ]
 
 MIDDLEWARE = [
