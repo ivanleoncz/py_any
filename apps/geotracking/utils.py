c652ba84e0688f5912be593dd2eba228db1b78b9
ivanleoncz <ivanlmj@gmail.com>
2022-02-04 21:33:38 -0600

    Improving geotracking utils (Class with Methods)
    Using now django-ipware for obtaining client ip addres, there's a a
    method for storing ip data, calling get_ip_data for searching ip data
    and then storing data on Visitors model.
    
    All of this is now structure in a Class called Utils, that can be
    inherited by all Views.

--- apps/geotracking/utils.py
+++ apps/geotracking/utils.py
@@ -1,22 +1,49 @@
 import json
+
+from django.utils import timezone
+from ipware import get_client_ip
 import requests
+
+from .models import Visitors
 
 ip_database_provider = "http://ipinfo.io/"
 
 
-def get_ip_data(ip_address: str) -> dict:
-    """
-    Searches and obtains IP metadata.
+class Utils:
 
-    parameters
-    ---------
-    ip_address : str
+    def get_ip_data(self, ip_address: str) -> dict:
+        """
+        Searches and obtains IP metadata.
 
-    returns
-    -------
-    IP address metadata, filtered via list comprehension over JSON response as dictionary.
-    """
-    r = requests.get("".join(ip_database_provider, ip_address))
-    # For returning more information on data dict, just add IPINFO fields on list comprehension's tuple...
-    data = {k: v for k, v in json.loads(r.content.decode()).items() if k in ('country', 'city', )}
-    return data
+        parameters
+        ---------
+        ip_address : str
+
+        returns
+        -------
+        IP address metadata, filtered via list comprehension over JSON response as dictionary.
+        """
+        r = requests.get("".join(ip_database_provider, ip_address))
+        # For returning more information on data dict, just add IPINFO fields on list comprehension's tuple...
+        data = {k: v for k, v in json.loads(r.content.decode()).items() if k in ('country', 'city', )}
+        return data
+
+    def store_ip_data(self, request: dict) -> None:
+        """
+        Stores the IP data at Visitors model, if any data is available via Utils.get_ip_data().
+
+        Parameters
+        ----------
+        request
+            Django default request object.
+        """
+        client_ip, is_routable = get_client_ip(request)
+        if is_routable:
+            data = self.get_ip_data(client_ip)  # Searching for IP data
+            if data:
+                visitor, created = Visitors.objects.get_or_create(ip=client_ip, country=data["country"],
+                                                                  city=data["city"], date=timezone.now())
+                if not created:  # If an entry already exists, then amount_of_requests is incremented
+                    visitor.amount_of_requests += 1
+                    visitor.save()
+
