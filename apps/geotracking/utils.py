f7cba546ab46a5fc7b227fbce620ebd4b9e64bf5
ivanleoncz <ivanlmj@gmail.com>
2022-03-24 20:22:53 -0600

    Fixing .join() parameter
    Two parameters were being passed, instead of one (tuple or list).
    URL (ipinfo + ipaddress) is being requested normally now.

--- apps/geotracking/utils.py
+++ apps/geotracking/utils.py
@@ -23,7 +23,7 @@ class Utils:
         -------
         IP address metadata, filtered via list comprehension over JSON response as dictionary.
         """
-        r = requests.get("".join(ip_database_provider, ip_address))
+        r = requests.get("".join((ip_database_provider, ip_address)))
         # For returning more information on data dict, just add IPINFO fields on list comprehension's tuple...
         data = {k: v for k, v in json.loads(r.content.decode()).items() if k in ('country', 'city', )}
         return data
