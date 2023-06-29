a305a53f8fb6a233cea33c0795ec568306fdbf25
ivanleoncz <ivanlmj@gmail.com>
2023-06-28 19:58:09 -0600

    feat: custom admin view for Visitor model
    In order to better present the visitor's data, also allowing searching
    over countries and cities.

--- apps/geotracking/admin.py
+++ apps/geotracking/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Visitor
 
+
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'country', 'city', 'amount_of_requests', 'last_request']
+    list_filter = 'country',
+    search_fields = ['country', 'city']
+    date_hierarchy = 'last_request'
+    empty_value_display = '-empty-'
+
+
 # Register your models here.
-admin.site.register(Visitor)
+admin.site.register(Visitor, VisitorAdmin)
