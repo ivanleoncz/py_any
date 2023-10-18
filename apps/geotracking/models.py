95e8431845bf7c90329318ed314a06a392d60120
ivanleoncz <ivanlmj@gmail.com>
2023-10-17 18:02:21 -0600

    fix: rename Visitor fields and add new field
    Names will better represent the business logic for this model and
    'referrer', will serve to register the websites which hosted the link to
    the application website (from where do the request came from?).

--- apps/geotracking/models.py
+++ apps/geotracking/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 from django_countries.fields import CountryField
@@ -5,16 +6,20 @@ from django_countries.fields import CountryField
 
 class Visitor(models.Model):
 
-    ip = models.GenericIPAddressField()
+    device_id = models.CharField(max_length=32,
+                                 help_text="Token generated upon request and stored as Persistent Cookie")
+    ip_address = models.GenericIPAddressField()
+    referrer = models.CharField(null=True, blank=True, max_length=256,
+                                help_text="Page which served the link to the request.")
     country = CountryField(null=True)
     city = models.CharField(null=True, max_length=64)
-    amount_of_requests = models.PositiveIntegerField(default=0)
-    last_request = models.DateTimeField(auto_now=True)
+    days_visited = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Visitor"
         verbose_name_plural = "Visitors"
 
     def __str__(self):
-        return f"{self.ip} from {self.country.name} ({self.city}): " \
-               f"{self.amount_of_requests} requests (last was {self.last_request})"
+        return f"{self.device_id} : {self.ip_address}"
